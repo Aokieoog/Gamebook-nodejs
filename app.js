d0d4033b1daef950b92b151813e7e854b4405bcc
aoki <aokieoog@gmail.com>
2025-01-06 10:03:42 +0800

    feat(server): 允许服务器在所有网络接口上监听

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.use(authMiddleware);
 app.use('/api', apiRoutes);
 
 // 启动服务器
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
