87c0bce432005b7c0c4f05d154f61d42e2b08769
aoki <aokieoog@gmail.com>
2025-01-06 19:51:39 +0800

    feat(cors): 添加支持凭证的 CORS 响应头

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', req.headers.origin||'https://jx.ieoog.com');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
