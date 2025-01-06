8e016d2214484dc49ce28e408fd815f065ad23a3
aoki <aokieoog@gmail.com>
2025-01-06 19:39:42 +0800

    feat(cors): 修改 CORS 设置以动态允许请求来源

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin||'jx.ieoog.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
